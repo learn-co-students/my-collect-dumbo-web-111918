24ab93d314fe08bf4107397aa6e20b2084793048
Roman Pirag <mr.anadale@yahoo.com>
2018-10-26 05:20:00 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,10 @@
-def my_collect(collection)
+def my_collect(array)
   i = 0
-  new_collection = []
-  while i < collection.length
-    new_collection << yield(collection[i])
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
     i += 1
   end
-  new_collection
+  collection
 end
 
- #array.map {|x| x.upcase}
